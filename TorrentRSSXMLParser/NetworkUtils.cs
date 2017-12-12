8e95577e102edd92f4e41b86bb3630a1eb81585b
James Vaughn <jimmyvau@gmail.com>
2017-12-11 21:39:43 -0600

    commit everything

---
+++ TorrentRSSXMLParser/NetworkUtils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TorrentXMLParser
+{
+    class NetworkUtils
+    {
+        string url;
+        private HttpClient client = new HttpClient();
+
+        public NetworkUtils()
+        {
+
+        }
+
+        public async Task<string> Get()
+        {
+            string body;
+            HttpResponseMessage result;
+
+            try
+            {
+                result = await client.GetAsync(url);
+
+                if (result.IsSuccessStatusCode)
+                {
+                    body = await result.Content.ReadAsStringAsync();
+                    return body;
+                }
+                else
+                {
+                    return result.StatusCode.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+
+            }
+
+        }
+
+        public string Url { get => url; set => url = value; }
+    }
+}
---
+++ TorrentRSSXMLParser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TorrentXMLParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(String[] args)
+        {
+            NetworkUtils utils = new NetworkUtils();
+
+            utils.Url = "";
+            string xml = await utils.Get();
+            Console.WriteLine(xml);
+            Console.ReadLine();
+        }
+
+    }
+}
+
